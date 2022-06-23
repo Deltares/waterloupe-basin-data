b8a929b48d6f1a256f103d2a1e25ed18d4683ad5
Wouter Schoonveld <35798126+wschoonveld@users.noreply.github.com>
2022-06-23 13:36:09 +0200

    db functions with json changed
    1. labels added
    2. extra json-function for the date level: scenariodata_per_date_json

--- dbimport/sql/create_object.sql
+++ dbimport/sql/create_object.sql
@@ -28,9 +28,9 @@ create table wl.scenariodata
     value   double precision
 );
 
-drop view if exists wl.scenariodata_agg cascade;
-create or replace view wl.scenariodata_agg as
-select fi.sub_parameter, fi.parameter, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.area, count(*) count_value, sum(value) sum_value
+drop view if exists wl.scenariodata_per_date cascade;
+create or replace view wl.scenariodata_per_date as
+select fi.sub_parameter, fi.parameter, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.date, sd.area, count(*) count_value, sum(value) sum_value
 from wl.scenariodata sd
 join (
     select case when parameter ilike '%%availability%%' then 'availability'
@@ -40,7 +40,20 @@ join (
     from wl.file
 ) fi on fi.file_id=sd.file_id
 join wl.period pe on sd.date between pe.start_date and pe.end_date
-group by fi.sub_parameter, fi.parameter, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.area
+group by fi.sub_parameter, fi.parameter, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.date, sd.area
+;
+
+drop view if exists wl.scenariodata_series_date cascade;
+create or replace view wl.scenariodata_series_date as
+select sub_parameter, parameter, scenario, solution, area, array_agg(sum_value order by date) as data, array_agg(distinct date order by date) as date_agg
+from  wl.scenariodata_per_date
+group by sub_parameter, parameter, scenario, solution, area;
+
+drop view if exists wl.scenariodata_agg cascade;
+create or replace view wl.scenariodata_agg as
+select sub_parameter, parameter, scenario, solution, period_id, period_name, area, count(*) count_value, sum(sum_value) sum_value
+from wl.scenariodata_per_date
+group by sub_parameter, parameter, scenario, solution, period_id, period_name, area
 ;
 
 drop view if exists wl.scenariodata_series_agg cascade;
@@ -53,8 +66,9 @@ drop function if exists wl.scenariodata_agg_json(selected_area varchar, selected
 create or replace function wl.scenariodata_agg_json(selected_area varchar, selected_scenario varchar, selected_solution varchar) returns setof json as
 $$
     select
-    json_object_agg(
-            'series',j.jdata
+   json_build_object(
+            'labels',to_json(pe.period_name_agg)
+            ,'series',j.jdata
     )
     from (
         select
@@ -62,15 +76,45 @@ $$
                 json_build_object(
                 'name', sda.parameter
                 ,'sub_parameter', sda.sub_parameter
---                 ,'period',sda.period_name
                 ,'data', to_json(sda.data)
---                 ,'data', to_json(sda.*)
                 )
         ) jdata
         from wl.scenariodata_series_agg sda
         where sda.area=selected_area and sda.scenario=selected_scenario and sda.solution=selected_solution
     ) j
+    , (select array_agg(period_name order by period_id) period_name_agg from wl.period) pe
 $$ language sql
 ;
 -- example:
--- select * from wl.scenariodata_agg_json('altoChillon', 'SSP3','none');
+-- select * from wl.scenariodata_agg_json('altoChillon', 'SSP2','none');
+
+
+drop function if exists wl.scenariodata_per_date_json(selected_area varchar, selected_scenario varchar, selected_solution varchar);
+create or replace function wl.scenariodata_per_date_json(selected_area varchar, selected_scenario varchar, selected_solution varchar) returns setof json as
+$$
+with x as (
+        select *
+        from wl.scenariodata_series_date sda
+        where sda.area=selected_area and sda.scenario=selected_scenario and sda.solution=selected_solution
+)
+    select
+   json_build_object(
+            'labels',to_json(da.date_agg)
+            ,'series',j.jdata
+    )
+    from (
+        select
+        json_agg(
+                json_build_object(
+                'name', x.parameter
+                ,'sub_parameter', x.sub_parameter
+                ,'data', to_json(x.data)
+                )
+        ) jdata
+        from x
+    ) j
+    , (select date_agg from x limit 1) da
+$$ language sql
+;
+-- example:
+-- select * from wl.scenariodata_per_date_json('altoChillon', 'SSP3','none');
