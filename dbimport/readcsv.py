dfbcc3e7ecae6119533023a048c1cd5496268be3
Wouter Schoonveld <35798126+wschoonveld@users.noreply.github.com>
2022-07-27 13:43:52 +0200

    import shp
    import shp file into table area

--- dbimport/readcsv.py
+++ dbimport/readcsv.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 from fileinput import filename
 from os import replace
-from numpy import empty
-from pandas.core.frame import DataFrame
-from pandas.io.pytables import Table
-from sqlalchemy.sql.expression import null
 import sys
 import csv
 from os.path import exists, basename, isfile
@@ -16,6 +12,7 @@ from sqlalchemy import text
 from pathlib import Path
 from glob import glob
 import os
+import geopandas as gpd
 
 def processWL(basePathSource, connString):
     # file_type = 'water*.csv' # exclude periods.csv
@@ -27,15 +24,21 @@ def processWL(basePathSource, connString):
     engine = create_engine(connString)
     conn = engine.connect()
 
+    # create db structure
     sqlfile = open(r'X:/waterloupe/import/sql/create_object.sql', 'r')
     sql_create_object = sqlfile.read()
     sqlfile.close()
     conn.execute(sql_create_object)
     db_schema = 'wl'
-
     df_file_columns = ['filename','parameter','scenario','solution']
     # df_file = pd.DataFrame(columns=df_file_columns)
 
+    # read shp file 
+    df_shp = gpd.read_file(r'D:\waterloupe\Peru\shp\areas.shp')
+    df_shp = df_shp.set_geometry('geometry')
+    df_shp.to_postgis('area', engine, schema=db_schema, index=True, index_label='Index')
+
+    # read csv files
     for csv_fil in csv_files:
         # read file name
         filename = f"{basename(csv_fil)[0 : -1 - 3]}"
@@ -73,7 +76,6 @@ def processWL(basePathSource, connString):
             dfm = dfm.melt(id_vars=['area'],var_name='date',value_name='value')
             dfm.insert(0, 'file_id',value=fileid)
             dfm.to_sql('scenariodata', engine, schema=db_schema, if_exists='append', index=False)
-
 
 # TO DO:
 # area->id?
