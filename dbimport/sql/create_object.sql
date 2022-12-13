f854785927fea28cb6b71c22e471cf320e41359c
Wouter Schoonveld <35798126+wschoonveld@users.noreply.github.com>
2022-12-13 13:35:34 +0100

    fix disappeared changes (added columns)

--- dbimport/sql/create_object.sql
+++ dbimport/sql/create_object.sql
@@ -51,7 +51,7 @@ create or replace view {db_schema}.list_solution as select distinct solution fro
 
 drop view if exists {db_schema}.scenariodata_per_date cascade;
 create or replace view {db_schema}.scenariodata_per_date as
-select fi.sub_parameter, fi.parameter, fi.users, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.date, a.area_id, a.name as area, count(*) count_value, sum(sd.value) as value
+select fi.sub_parameter, fi.parameter, fi.users, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.date, a.area_id, a.name as area, a.km2 as area_km2, a.geometry, count(*) count_value, sum(sd.value) as value
 from {db_schema}.scenariodata sd
 join {db_schema}.area a on a.area_id=sd.area_id
 join (
@@ -63,7 +63,7 @@ join (
     from {db_schema}.file f
 ) fi on fi.file_id=sd.file_id
 join {db_schema}.period pe on sd.date between pe.start_date and pe.end_date
-group by fi.sub_parameter, fi.parameter, fi.users, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.date, a.area_id, a.name;
+group by fi.sub_parameter, fi.parameter, fi.users, fi.scenario, fi.solution, pe.period_id, pe.period_name, sd.date, a.area_id, a.km2, a.geometry, a.name;
 
 drop view if exists {db_schema}.scenariodata_series_date cascade;
 create or replace view {db_schema}.scenariodata_series_date as
@@ -73,9 +73,9 @@ group by sub_parameter, parameter, scenario, solution, area_id, area;
 
 drop view if exists {db_schema}.scenariodata_agg cascade;
 create or replace view {db_schema}.scenariodata_agg as
-select sub_parameter, parameter, users, scenario, solution, period_id, period_name, area_id, area, count(*) count_value, sum(value) as value
+select sub_parameter, parameter, users, scenario, solution, period_id, period_name, area_id, area, area_km2, count(*) count_value, sum(value) as value, geometry
 from {db_schema}.scenariodata_per_date
-group by sub_parameter, parameter, users, scenario, solution, period_id, period_name, area_id, area;
+group by sub_parameter, parameter, users, scenario, solution, period_id, period_name, area_id, area, area_km2, geometry;
 
 drop view if exists {db_schema}.scenariodata_series_agg cascade;
 create or replace view {db_schema}.scenariodata_series_agg as
@@ -89,12 +89,22 @@ select * from {db_schema}.scenariodata_series_date where parameter in ('waterAva
 
 drop view if exists {db_schema}.scenariodata_per_period cascade;
 create or replace view {db_schema}.scenariodata_per_period as
-select fi.parameter, fi.filename, fi.users, fi.scenario, fi.solution, pe.period_id, pe.period_name, ar.area_id, ar.name as area, ar.km2 as area_km2, sc.value as value, ar.geometry
-, 'The risk for {name} is {value}.' as "popupHTML"
-from {db_schema}.scenariodata sc
-join {db_schema}.file fi on fi.file_id=sc.file_id
-join {db_schema}.period pe on pe.period_id=sc.period_id
-join {db_schema}.area ar on ar.area_id=sc.area_id
+with data_with_period as(
+    select fi.parameter, fi.users, fi.scenario, fi.solution, pe.period_id, pe.period_name, ar.area_id, ar.name as area, ar.km2 as area_km2, sc.value, ar.geometry
+    , 'The risk for {name} is {value}.' as "popupHTML"
+    from {db_schema}.scenariodata sc
+    join {db_schema}.file fi on fi.file_id=sc.file_id
+    join {db_schema}.period pe on pe.period_id=sc.period_id
+    join {db_schema}.area ar on ar.area_id=sc.area_id
+)
+, data_with_date as (
+    select parameter, users, scenario, solution, period_id, period_name, area_id, area, area_km2, value, geometry
+        , 'The risk for {name} is {value}.' as "popupHTML"
+    from {db_schema}.scenariodata_agg sd
+)
+select * from data_with_period
+union all
+select * from data_with_date
 ;
 
 drop view if exists {db_schema}.scenariodata_risk_per_period cascade;
